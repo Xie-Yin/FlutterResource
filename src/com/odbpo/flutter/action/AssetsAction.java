2c3f9a885751c4cd3276b25d9d7022268585f90c
azhon <958460248@qq.com>
2022-11-22 15:46:30 +0800

    ignore eg:1.5x 2.0x dir

--- src/com/odbpo/flutter/action/AssetsAction.java
+++ src/com/odbpo/flutter/action/AssetsAction.java
@@ -15,6 +15,9 @@ import com.odbpo.flutter.util.StringUtil;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 /**
  * createDate: 2022/11/22 on 09:34
@@ -31,8 +34,17 @@ public class AssetsAction extends AnAction {
         Project project = event.getData(PlatformDataKeys.PROJECT);
         if (project == null) return;
         Collection<VirtualFile> assetsFiles = FileUtil.getDirFile(project, Constants.ASSETS_DIR);
-        if (assetsFiles == null || assetsFiles.isEmpty()) {
-            //没有需要生成的资源文件
+        if (assetsFiles == null) {
+            NotificationUtil.showNotify("No assets file need to be generate！");
+            return;
+        }
+        //需要过滤一下分辨率文件夹（1.5x，2.0x，3.0x）不进行生成
+        assetsFiles = assetsFiles.stream().filter(file -> {
+            String pattern = "\\d.\\dx";
+            Matcher matcher = Pattern.compile(pattern).matcher(file.getPath());
+            return !matcher.find();
+        }).collect(Collectors.toList());
+        if (assetsFiles.isEmpty()) {
             NotificationUtil.showNotify("No assets file need to be generate！");
             return;
         }
